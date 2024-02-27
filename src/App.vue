66d2f53cbca3ff57c19fa19eed57005939d864f1
Pablo Cubillos <pcubillos326@gmail.com>
2024-02-27 12:32:51 -0500

    adding Patient component to show in a list

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,45 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, reactive } from 'vue';
 import Header from './components/Header.vue'
 import Form from './components/Form.vue'
+import Patient from './components/Patient.vue'
 
 const patients = ref([])
+
+const patient = reactive({
+  name: '',
+  owner: '',
+  email: '',
+  clearanceDate: '',
+  symptoms: ''
+})
+
+const savePatient = () => {
+  patients.value.push({ ...patient })
+  patient.name = ''
+  patient.owner = ''
+  patient.email = ''
+  patient.clearanceDate = ''
+  patient.symptoms = ''
+}
 </script>
 
 <template>
   <div class="container mx-auto mt-20 px-4">
     <Header />
     <div class="mt-12 md:flex">
-      <Form />
+      <Form v-model:name="patient.name" v-model:owner="patient.owner" v-model:email="patient.email"
+        v-model:clearanceDate="patient.clearanceDate" v-model:symptoms="patient.symptoms" @save-patient="savePatient" />
       <div class="md:w-1/2 md:h-screen overflow-y-scroll">
         <h3 class="font-black text-3xl text-center">Check your patients</h3>
 
-        <div v-if="patients.length > 0"></div>
+        <div v-if="patients.length > 0">
+          <p class="text-lg mt-5 text-center mb-10">
+            Patients
+            <span class="text-indigo-600 font-bold">Information</span>
+          </p>
+          <Patient v-for="patient in patients" :patient="patient" />
+        </div>
         <p v-else class="mt-20 text-2xl text-center">There aren't patients</p>
       </div>
     </div>
--- src/components/Form.vue
+++ src/components/Form.vue
@@ -2,25 +2,53 @@
 import { reactive } from 'vue';
 import Alert from './Alert.vue'
 
+const emit = defineEmits(['update:name', 'update:owner', 'update:email', 'update:clearanceDate', 'update:symptoms', 'save-patient'])
+
+const props = defineProps({
+  name: {
+    type: String,
+    required: true
+  },
+  owner: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  clearanceDate: {
+    type: String,
+    required: true
+  },
+  symptoms: {
+    type: String,
+    required: true
+  }
+})
+
 const alert = reactive({
   type: '',
   message: ''
 })
 
-const patient = reactive({
-  name: '',
-  owner: '',
-  email: '',
-  clearanceDate: '',
-  symptoms: ''
-})
-
 const onSubmit = () => {
-  if (Object.values(patient).includes('')) {
+  if (Object.values(props).includes('')) {
     alert.message = 'All fields are required'
     alert.type = 'error'
     return
   }
+
+  emit('save-patient')
+  alert.message = 'Patient saved succesfully'
+  alert.type = 'success'
+
+  setTimeout(() => {
+    Object.assign(alert, {
+      type: '',
+      message: ''
+    })
+  }, 3000)
 }
 </script>
 
@@ -37,26 +65,29 @@ const onSubmit = () => {
       <div class="mb-5">
         <label class="block text-gray-700 uppercase font-bold" for="pet">Pet Name</label>
         <input id="pet" type="text" placeholder="Pet" class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
-          v-model="patient.name" />
+          @input="$emit('update:name', $event.target.value)" :value="name" />
       </div>
       <div class="mb-5">
         <label class="block text-gray-700 uppercase font-bold" for="owner">Owner's Name</label>
         <input id="owner" type="text" placeholder="Owner" class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
-          v-model="patient.owner" />
+          @input="$emit('update:owner', $event.target.value)" :value="owner" />
       </div>
       <div class="mb-5">
         <label class="block text-gray-700 uppercase font-bold" for="email">Email</label>
         <input id="email" type="email" placeholder="Owner's email"
-          class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" v-model="patient.email" />
+          class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+          @input="$emit('update:email', $event.target.value)" :value="email" />
       </div>
       <div class="mb-5">
         <label class="block text-gray-700 uppercase font-bold" for="clearance">Medical Clearance</label>
-        <input id="cleareance" type="date" class="border-2 w-full p-2 mt-2 rounded-md" v-model="patient.clearanceDate" />
+        <input id="cleareance" type="date" class="border-2 w-full p-2 mt-2 rounded-md"
+          @input="$emit('update:clearanceDate', $event.target.value)" :value="clearanceDate" />
       </div>
       <div class="mb-5">
         <label class="block text-gray-700 uppercase font-bold" for="symptoms">Symptoms</label>
         <textarea id="symptoms" placeholder="Describe the symptoms"
-          class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md h-32" v-model="patient.symptoms" />
+          class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md h-32"
+          @input="$emit('update:symptoms', $event.target.value)" :value="symptoms" />
       </div>
       <input type="submit"
         class="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-colors"
